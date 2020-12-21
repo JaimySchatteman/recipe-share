1be8f255f0ce670d147f3d667e62c2f95a92a32f
jaimy <schattemanjaimy@outlook.com>
2020-12-21 14:30:54 +0100

    🎉 Setup initial project files
    Initialize Vue project, add empty components, install tailwindcss

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-1/6 bg-green-100">
+    <img src="" alt="">
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
